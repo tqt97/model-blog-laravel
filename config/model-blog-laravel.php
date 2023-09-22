da30d0c8cfca503d22671152965ca233c3274581
QuocTuanIT <kutuanonline199@gmail.com>
2023-09-22 09:04:42 +0700

    add model

--- config/model-blog-laravel.php
+++ config/model-blog-laravel.php
@@ -2,5 +2,14 @@
 
 // config for Tqt97/ModelBlogLaravel
 return [
-
+    /*
+        *  List tables in your package
+        *  you can change table name in this config if you want
+        */
+    'table_names' => [
+        'articles' => 'articles',
+        'categories' => 'categories',
+        'tags' => 'tags',
+        'pivot' => 'article_tag'
+    ],
 ];
