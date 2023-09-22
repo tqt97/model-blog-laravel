2c874173014fefcd6dcad0eb2c638dc7bd38762d
tqt97 <tqt97@users.noreply.github.com>
2023-09-22 03:18:01 +0000

    Fix styling

--- src/ModelBlogLaravelServiceProvider.php
+++ src/ModelBlogLaravelServiceProvider.php
@@ -3,11 +3,11 @@
 namespace Tqt97\ModelBlogLaravel;
 
 use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Tqt97\ModelBlogLaravel\Commands\ModelBlogLaravelCommand;
-use Illuminate\Filesystem\Filesystem;
-use Illuminate\Support\Collection;
 
 class ModelBlogLaravelServiceProvider extends PackageServiceProvider
 {
@@ -39,26 +39,27 @@ class ModelBlogLaravelServiceProvider extends PackageServiceProvider
      */
     protected function offerPublishing(): void
     {
-        if (!$this->app->runningInConsole()) {
+        if (! $this->app->runningInConsole()) {
             return;
         }
 
-        if (!function_exists('config_path')) {
+        if (! function_exists('config_path')) {
             // function not available and 'publish' not relevant in Lumen
             return;
         }
 
         $this->publishes([
-            __DIR__ . '/../config/permission.php' => config_path('permission.php'),
+            __DIR__.'/../config/permission.php' => config_path('permission.php'),
         ], 'permission-config');
 
         $this->publishes([
-            __DIR__ . '/../database/migrations/create_model-blog-laravel_table.php.stub' => $this->getMigrationFileName('create_model-blog-laravel_table.php'),
+            __DIR__.'/../database/migrations/create_model-blog-laravel_table.php.stub' => $this->getMigrationFileName('create_model-blog-laravel_table.php'),
         ], 'permission-migrations');
     }
 
     /**
      * Returns existing migration file if found, else uses the current timestamp.
+     *
      * @throws BindingResolutionException
      */
     protected function getMigrationFileName(string $migrationFileName): string
@@ -67,9 +68,9 @@ class ModelBlogLaravelServiceProvider extends PackageServiceProvider
 
         $filesystem = $this->app->make(Filesystem::class);
 
-        return Collection::make([$this->app->databasePath() . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR])
-            ->flatMap(fn($path) => $filesystem->glob($path . '*_' . $migrationFileName))
-            ->push($this->app->databasePath() . "/migrations/{$timestamp}_{$migrationFileName}")
+        return Collection::make([$this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR])
+            ->flatMap(fn ($path) => $filesystem->glob($path.'*_'.$migrationFileName))
+            ->push($this->app->databasePath()."/migrations/{$timestamp}_{$migrationFileName}")
             ->first();
     }
 }
